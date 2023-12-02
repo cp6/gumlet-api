6a02698c81d00c0e46599ba6dfd04054677d85eb
cp6 <cp6@users.noreply.github.com>
2023-12-02 23:49:23 +1100

    Updated index.php

--- index.php
+++ index.php
@@ -5,7 +5,10 @@ use Corbpie\Gumlet;
 
 $gm = new Gumlet\GumletVideo();
 
+//Setting video collection
 $gm->video_collection = '64b8c2bf2c2bc76a3c83f096';
+
+//Setting video ID
 $gm->video_id = '64bbf923a0f8d93c55f09ba0';
 
 echo json_encode($gm->listProfiles());
