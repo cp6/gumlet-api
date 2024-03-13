44ec6333baa318be40ebba2cd102a6935da14d61
cp6 <cp6@users.noreply.github.com>
2024-03-13 23:02:09 +1100

    Added create video example to index.php

--- index.php
+++ index.php
@@ -9,6 +9,38 @@ $gm = new Gumlet\GumletVideo();
 $gm->video_collection = '64b8c2bf2c2bc76a3c83f096';
 
 //Setting video ID
-$gm->video_id = '64bbf923a0f8d93c55f09ba0';
+//$gm->video_id = '64bbf923a0f8d93c55f09ba0';
 
-echo json_encode($gm->listProfiles());
+//echo json_encode($gm->listProfiles());
+
+
+//Creating (adding) a new video with extra audio channels
+$parameters = [
+    'profile_id' => '65138fa952ccfd817db2a665',
+    'title' => 'The title for this great video',
+    'description' => 'The description for this great video',
+    'mp4_access' => false,
+    'audio_only' => false,
+    'additional_tracks' => [
+        [
+            'url' => 'https://gumlet.sgp1.digitaloceanspaces.com/video/sample_1.aac',
+            'type' => 'audio',
+            'language_code' => 'en',
+            'name' => 'English'],
+        [
+            'url' => 'https://gumlet.sgp1.digitaloceanspaces.com/video/sample_1.aac',
+            'type' => 'audio',
+            'language_code' => 'pl',
+            'name' => 'Polish'
+        ]
+    ]
+];
+
+$video = $gm->createVideoFromUrl(
+    'https://domain.com/video.mp4',
+    'MP4',
+    $parameters
+);
+
+
+echo json_encode($video);
